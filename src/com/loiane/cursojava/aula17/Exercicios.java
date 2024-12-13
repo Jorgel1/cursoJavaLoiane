02d26b9ab6a1212f9f1bf1f0301f1431f94b2205
jorramos <95020694+Jorgel1@users.noreply.github.com>
2024-12-13 08:32:38 +0000

    respondi exercicio 3

--- src/com/loiane/cursojava/aula17/Exercicios.java
+++ src/com/loiane/cursojava/aula17/Exercicios.java
@@ -17,8 +17,53 @@ public class Exercicios {
             }
         } while(nota < 0 || nota > 10);
 
-         */
+         //3 nome de usuário e senha:
+        System.out.println("Informe o nome do usuário:");
         String nomeUsuario = scan.next();
+        String senhaUsuario;        
+        do {
+            System.out.println("Informe a senha do usuário:");
+            senhaUsuario = scan.next();
+            if (nomeUsuario.equals(senhaUsuario)) {
+                System.out.println("Erro");
+            }
+        }while (nomeUsuario.equals(senhaUsuario));
+        System.out.println("Usuário conectado ...");
+
+        */
+
+        //4 validar 5 informações:
+
+        System.out.println("Valide as seguintes informacoes: ");
+        String nome;
+        int idade;
+        double salario;
+        String sexo;
+        String estadoCivil;
+        do {
+            System.out.println("Nome: maior que 3 caracteres");
+            nome = scan.next();
+        }while (nome.length() <= 3);
+
+        do {            
+            System.out.println("Idade: entre 0 e 150");
+            idade = scan.nextInt();
+        }while (idade >= 0 || idade <= 150);   
+
+        do {
+            System.out.println("Salario: maior que zero" );
+            salario = scan.nextDouble();
+        }while (salario > 0);  
+
+        do {
+            System.out.println("Sexo: 'f' ou 'm.'");
+
+        }while (sexo.equalsIgnoreCase("f") || sexo.equalsIgnoreCase("m"));
+            
+        do {
+            System.out.println("Estado Civil: 's', 'c', 'v', 'd'");
+        }while (estadoCivil.contains('s') || estadoCivil.contains("c") || estadoCivil.contains("v") || estadoCivil.contains("d"));  
+            
         
 
         
