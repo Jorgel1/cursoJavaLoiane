ca954e7adf0b07f6cf15ec1a369703098d9c3f1f
Jorgel1 <jorgegothicmetal@gamail.com>
2024-12-13 16:49:09 -0300

    Finalizado exerc 5

--- src/com/loiane/cursojava/aula17/Exercicios.java
+++ src/com/loiane/cursojava/aula17/Exercicios.java
@@ -30,7 +30,7 @@ public class Exercicios {
         }while (nomeUsuario.equals(senhaUsuario));
         System.out.println("Usuário conectado ...");
 
-        */
+
 
         //4 validar 5 informações:
 
@@ -55,22 +55,80 @@ public class Exercicios {
             salario = scan.nextDouble();
         }while (salario <= 0);  
 
-        /*
+
         do {
             System.out.println("Sexo: 'f' ou 'm.'");
             sexo = scan.next();
 
-        }while (!sexo.equalsIgnoreCase("f") || !sexo.equalsIgnoreCase("m"));
-            */
+        }while ((!sexo.equalsIgnoreCase("f")) || (!sexo.equalsIgnoreCase("m")));
+
         
         do {
             System.out.println("Estado Civil: 's', 'c', 'v', 'd'");
             estadoCivil = scan.next();
-        }while (estadoCivil.contains("s") || estadoCivil.contains("c") || estadoCivil.contains("v") || estadoCivil.contains("d"));  
-            
-        
+        }while ((!estadoCivil.contains("s")) || estadoCivil.contains("c") || estadoCivil.contains("v") || estadoCivil.contains("d"));
 
-        
+
+
+        //4 quantos anos para igualar a populaçao
+
+        int populacaoA = 80000;
+        int populacaoB = 200000;
+        int anos = 0;
+
+        while (populacaoA <= populacaoB){
+            populacaoA += populacaoA/100 * 3;
+            populacaoB += populacaoA/100 * 1.5;
+            anos++;
+        }
+
+        System.out.println("Quantidade de anos necessários: " + anos);
+        System.out.println("População A " + populacaoA);
+        System.out.println("População B " + populacaoB);
+
+
+        //5 quantos anos para igualar a populaçao, o usuario que escolhe os dados:
+
+
+        int populacaoA;
+        int populacaoB;
+        double percentA;
+        double percentB;
+               int anos = 0;
+           do {
+               System.out.println("Informe a população A: ");
+               populacaoA = scan.nextInt();
+           }while (populacaoA < 0);
+
+        do {
+            System.out.println("Informe a população B: ");
+            populacaoB = scan.nextInt();
+            }while (populacaoB < 0);
+
+        do {
+            System.out.println("Informe o percentual A: ");
+            percentA = scan.nextDouble();
+        }while (percentA < 0);
+
+        do {
+            System.out.println("Informe o percentual B: ");
+            percentB = scan.nextDouble();
+        }while (percentB < 0);
+
+
+       while (populacaoA <= populacaoB){
+           populacaoA += populacaoA/100 * percentA;
+          populacaoB += populacaoA/100 * percentB;
+           anos++;
+        }
+
+        System.out.println("Quantidade de anos necessários: " + anos);
+        System.out.println("População A " + populacaoA);
+        System.out.println("População B " + populacaoB);
+
+
+ */
+
     }
     
 }
