c5e88f40d356671ea2e91bb6c08fec84ccf78e42
jorramos <95020694+Jorgel1@users.noreply.github.com>
2024-12-25 06:54:50 +0000

    parei no exercício 7

---
+++ src/com/loiane/cursojava/aula19/ExerciciosAula19.java
@@ -0,0 +1,84 @@
+package com.loiane.cursojava.aula19;
+
+import java.util.Scanner;
+
+public class ExerciciosAula19 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //1 criar vetorA com 5 posições e com valores e 
+        //Passar para VetorB com 5 posições.
+        /* 
+        int[] vetorA = new int[5];
+        
+        vetorA[0] = 10; 
+        vetorA[1] = 20; 
+        vetorA[2] = 30; 
+        vetorA[3] = 40; 
+        vetorA[4] = 50; 
+
+        int[] vetorB = new int[5];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = vetorA[i];
+        }
+
+        System.out.println("VetorB:");
+        for (int i : vetorB) {
+            System.out.println(i);
+        }
+            */
+
+        //2 vetorB de 8 posições inteiros recebe o dobro do vetorA .
+
+        /*
+        int[] vetorA = new int[8];
+        
+        vetorA[0] = 5; 
+        vetorA[1] = 10; 
+        vetorA[2] = 15; 
+        vetorA[3] = 20; 
+        vetorA[4] = 25; 
+        vetorA[5] = 30; 
+        vetorA[6] = 35; 
+        vetorA[7] = 40; 
+
+        int[] vetorB = new int[8];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = vetorA[i] * 2;
+        }
+
+        System.out.println("VetorB:");
+        for (int i : vetorB) {
+            System.out.println(i);
+        }
+             */
+
+        //3 vetor de 15 elementos recebendo o dobro de outros vetores.
+
+        /*
+        int[] vetorA = new int[15];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] = (i + 1);
+        }
+        
+
+        int[] vetorB = new int[15];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = vetorA[i] * vetorA[i];
+        }
+
+        System.out.println("VetorB:");
+        for (int i : vetorB) {
+            System.out.println(i);
+        }
+             */
+
+             //parei no exercício 7
+            //4
+
+    }
+}
