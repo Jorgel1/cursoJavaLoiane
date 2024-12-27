355a6b25047667ad996938c1ab533f17b411b793
jorramos <95020694+Jorgel1@users.noreply.github.com>
2024-12-27 07:10:04 +0000

    exercicio 20

--- src/com/loiane/cursojava/aula19/ExerciciosAula19.java
+++ src/com/loiane/cursojava/aula19/ExerciciosAula19.java
@@ -27,11 +27,11 @@ public class ExerciciosAula19 {
         for (int i : vetorB) {
             System.out.println(i);
         }
-            */
+            
 
         //2 vetorB de 8 posições inteiros recebe o dobro do vetorA .
 
-        /*
+        
         int[] vetorA = new int[8];
         
         vetorA[0] = 5; 
@@ -53,11 +53,11 @@ public class ExerciciosAula19 {
         for (int i : vetorB) {
             System.out.println(i);
         }
-             */
+             
 
         //3 vetor de 15 elementos recebendo o dobro de outros vetores.
 
-        /*
+       
         int[] vetorA = new int[15];
 
         for (int i = 0; i < vetorA.length; i++) {
@@ -75,10 +75,381 @@ public class ExerciciosAula19 {
         for (int i : vetorB) {
             System.out.println(i);
         }
-             */
+             
 
              //parei no exercício 7
-            //4
+            //4 vetorB receber o dobro do vetorA de 15 elementos.
+
+               double[] vetorA = new double[15];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] = (i + 1) * (i + 1);
+        }
+        
+
+        double[] vetorB = new double[15];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorB[i] = Math.sqrt(vetorA[i]);            
+        }
+
+        System.out.println("VetorB:");
+        for (double i : vetorB) {
+            System.out.println(i);
+        }
+            
+            //5 vetorB = vetorA * i
+        
+            int[] vetorA = new int[10];
+
+            for (int i = 0; i < vetorA.length; i++) {
+                vetorA[i] = (i + 1);
+            }
+            
+    
+            int[] vetorB = new int[10];
+    
+            for (int i = 0; i < vetorA.length; i++) {
+                vetorB[i] = vetorA[i] * i;            
+            }
+    
+            System.out.println("VetorB:");
+            for (int i : vetorB) {
+                System.out.println(i);
+            }
+        
+                    //6 vetorC = vetorB + vetorA
+        
+                    int[] vetorA = new int[10];
+                    int[] vetorB = new int[10];
+                    int[] vetorC = new int[10];
+
+                    for (int i = 0; i < vetorA.length; i++) {
+                        vetorA[i] = (i + 1);                       
+                    }
+                    
+            
+                    for (int i = 0; i < vetorB.length; i++) {
+                        vetorB[i] = vetorA[i] * i;            
+                    }
+
+                    for (int i = 0; i < vetorC.length; i++) {
+                        vetorC[i] = vetorA[i] + vetorB[i];            
+                    }
+
+            
+                    System.out.println("VetorC:");
+                    for (int i : vetorC) {
+                        System.out.println(i);
+                    }
+                        
+                    //7 vetorC = vetorB - vetorA
+        
+                    int[] vetorA = new int[10];
+                    int[] vetorB = new int[10];
+                    int[] vetorC = new int[10];
+
+                    for (int i = 0; i < vetorA.length; i++) {
+                        vetorA[i] = (i + 1);                       
+                    }
+                    
+            
+                    for (int i = 0; i < vetorB.length; i++) {
+                        vetorB[i] = vetorA[i] * i;            
+                    }
+
+                    for (int i = 0; i < vetorC.length; i++) {
+                        vetorC[i] = vetorA[i] - vetorB[i];            
+                    }
+
+            
+                    System.out.println("VetorC:");
+                    for (int i : vetorC) {
+                        System.out.println(i);
+                    }
+                
+                      //8 vetorC = vetorB * vetorA
+        
+                      int[] vetorA = new int[10];
+                      int[] vetorB = new int[10];
+                      int[] vetorC = new int[10];
+  
+                      for (int i = 0; i < vetorA.length; i++) {
+                          vetorA[i] = (i + 1);                       
+                      }
+                      
+              
+                      for (int i = 0; i < vetorB.length; i++) {
+                          vetorB[i] = vetorA[i] * i;            
+                      }
+  
+                      for (int i = 0; i < vetorC.length; i++) {
+                          vetorC[i] = vetorA[i] * vetorB[i];            
+                      }
+  
+              
+                      System.out.println("VetorC:");
+                      for (int i : vetorC) {
+                          System.out.println(i);
+                      }
+                      
+                          //9 vetorC = vetorB / vetorA
+        
+                          int[] vetorA = new int[10];
+                          int[] vetorB = new int[10];
+                          int[] vetorC = new int[10];
+      
+                          for (int i = 0; i < vetorB.length; i++) {
+                              vetorB[i] = (i + 1) * 2;                       
+                          }
+                          
+                  
+                          for (int i = 0; i < vetorA.length; i++) {
+                              vetorA[i] = vetorB[i] * (i + 1);            
+                          }
+      
+                          for (int i = 0; i < vetorC.length; i++) {
+                              vetorC[i] = vetorA[i] / vetorB[i];            
+                          }
+      
+                  
+                          System.out.println("VetorC:");
+                          for (int i : vetorC) {
+                              System.out.println(i);
+                          }
+                              
+
+                        //10 vetorB = vetorA % 2
+        
+                          int[] vetorA = new int[10];
+                          int[] vetorB = new int[10];
+                         
+      
+                          for (int i = 0; i < vetorA.length; i++) {
+                              vetorA[i] =  (i + 1);            
+                          }
+
+                          for (int i = 0; i < vetorB.length; i++) {
+                              vetorB[i] = vetorA[i] % 2;                       
+                          }
+                          
+                  
+                          System.out.println("VetorB:");
+                          for (int i : vetorB) {
+                              System.out.println(i);
+                          }
+                              
+
+                              
+                        //11 a quantidade de elementos neste vetor
+        
+                          int[] vetorA = new int[10];                          
+                         
+      
+                          for (int i = 0; i < vetorA.length; i++) {
+                              vetorA[i] =  (i + 1) * 2;            
+                          }                        
+                  
+                   System.out.println(vetorA.length);
+                   
+
+                //12 a soma dos elementos neste vetor
+                    
+                int[] vetorA = new int[10];    
+                int soma = 0;                      
+                                    
+                
+                for (int i = 0; i < vetorA.length; i++) {
+                    vetorA[i] =  (i + 1) * 2;            
+                }                        
+
+            for (int i : vetorA) {
+                soma += i;
+            }
+
+            System.out.println(soma);
+               
+                //13 soma dos vetores múltiplos de 5
+      
+                int[] vetorA = new int[10];    
+                int soma = 0;                      
+                                    
+                
+                for (int i = 0; i < vetorA.length; i++) {
+                    vetorA[i] =  (i + 1);            
+                }                        
+
+            for (int i : vetorA) {
+                if (i % 5 == 0) {
+                    soma += i;                    
+                }
+            }
+
+            System.out.println(soma);
+            
+        //14 media aritmética dos ímpares
+            
+        int[] vetorA = new int[10];  
+        int soma = 0;
+        int cont = 0;  
+        int mediaAritmetica;                      
+                            
+        
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] =  (i + 1);            
+        }                        
+
+        for (int i : vetorA) {
+        if (i % 2 != 0) {
+            soma += i; 
+            cont++;                  
+        }
+        }
+        System.out.println("soma " + soma);
+        System.out.println("cont " + cont);
+        mediaAritmetica = soma / cont;
+
+        System.out.println("Média " + mediaAritmetica);
+       
+        //15 calcular o percentual de pares e ímpares
+
+        int[] vetorA = new int[10]; 
+        int contImpar = 0; 
+        int contPar = 0; 
+        int percentImpar;
+        int percentPar;                        
+        
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] =  (i + 1);            
+        }                        
+
+        for (int i : vetorA) {
+            if (i % 2 != 0) {
+                contImpar++;
+            } else {
+                contPar++;
+            }
+        }
+
+        percentPar = contPar * 10;
+        percentImpar = contImpar * 10;
+        System.out.println("percentual par " + percentPar + "%");
+        System.out.println("percentual ímpar " + percentImpar + "%");
+       
+
+        //16 soma dos vetores menores que 15,
+        // quantidade que é maior q 15, media maiores que 15
+
+        int[] vetorA = new int[10]; 
+        int somaMenor = 0; 
+        int somaMaior = 0; 
+        int qtd = 0; 
+        int media;
+        int cont = 0;
+                               
+        
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] =  (i + 1) * 3;            
+        }                        
+
+        for (int i : vetorA) {
+            if (i < 15) {
+                somaMenor += i;
+            } if(i == 15) {
+                qtd++;
+            } if (i > 15) {
+                cont++;
+                somaMaior += i;
+            }
+        }
+
+        media = somaMaior / cont;
+        System.out.println("Soma dos vetores inferiores a 15= " + somaMenor);
+        System.out.println("Quantidade dos vetores iguais a 15= " + qtd);
+        System.out.println("Média dos vetores superiores a 15= " + media);
+        
+
+        //17 quantidade de pessoas acima de 35 anos.
+
+        int[] vetorA = new int[10];       
+        int cont = 0; 
+        
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] =  (i + 1) * 8;            
+        }                        
+
+        for (int i : vetorA) {
+            if (i > 35) {
+                cont++;
+            } 
+        }
+        System.out.println("Quantidade de pessoas acima dos 35 anos: " +  cont);
+       
+
+        //18 a menor idade, maior idade e suas posições.
+
+        
+        int[] vetorA = new int[10];       
+        int menorIdade = Integer.MAX_VALUE;
+        int maiorIdade = Integer.MIN_VALUE;
+        int posicaoMenor = 0;
+        int posicaoMaior = 0; 
+        
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorA[i] =  (i + 1) * 8;            
+        }  
+
+        for (int i = 0; i < vetorA.length; i++) {
+            if (vetorA[i] < menorIdade) {
+                menorIdade = vetorA[i];
+                posicaoMenor = i;
+            }
+            if (vetorA[i] > maiorIdade) {
+                maiorIdade = vetorA[i];
+                posicaoMaior = i;
+            }
+        }
+
+        System.out.println("Menor idade "+ menorIdade + " na posição " + posicaoMenor);
+        System.out.println("Maior idade " + maiorIdade + " na posição " + posicaoMaior); 
+        
+
+        //19  calcular a media e se for maior que 7 o aluno está aprovado
+        // menor que 7 o aluno está reprovado.
+
+        double[] nota1 = new double[10];
+        double[] nota2 = new double[10];
+        double[] result = new double[10];
+
+        for (int i = 0; i < nota1.length; i++) {
+        System.out.println("Informe a nota1 na posição " + i);
+        nota1[i] = scan.nextDouble();
+
+        System.out.println("Informe a nota2 na posição " + i);
+        nota2[i] = scan.nextDouble();
+
+        result[i] = (nota1[i] + nota2[i]) / 2;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] >= 7) {
+                System.out.println("O aluno " + i  +" está aprovado com " + result[i] + " pontos" );
+            } else {
+                System.out.println("O aluno " + i  +" está reprovado com " + result[i] + " pontos" );
+            }
+
+        }
+        */
+        //20 vetor com 20 elementos
+        double[] vetorA = new double[20];
+        double cotacaoDollar = 6.14;
+
+        for (int i = 1; i <= vetorA.length; i++) {
+            vetorA[i] = cotacaoDollar * i;
+            System.out.println(i +" Dollar: R$ " + vetorA[i]);
+        }
+
+       
 
     }
 }
