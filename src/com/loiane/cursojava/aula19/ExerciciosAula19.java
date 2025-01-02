07c991fc0ce26fc2f5229f65b1866da6bf941ece
jorramos <95020694+Jorgel1@users.noreply.github.com>
2025-01-02 08:47:58 +0000

    exercicio 18 corrigido

--- src/com/loiane/cursojava/aula19/ExerciciosAula19.java
+++ src/com/loiane/cursojava/aula19/ExerciciosAula19.java
@@ -251,6 +251,10 @@ public class ExerciciosAula19 {
                           }                        
                   
                    System.out.println(vetorA.length);
+
+                   //versao da professora
+
+
                    
 
                 //12 a soma dos elementos neste vetor
@@ -412,6 +416,41 @@ public class ExerciciosAula19 {
 
         System.out.println("Menor idade "+ menorIdade + " na posição " + posicaoMenor);
         System.out.println("Maior idade " + maiorIdade + " na posição " + posicaoMaior); 
+        
+
+        //versao da professora
+
+              int[] idades = new int[10];
+            
+            for (int i = 0; i < idades.length; i++) {
+                System.out.println("Entre com a idade da pessoa " + (i+i));
+                idades[i] = scan.nextInt();
+            }
+
+            int menor = idades[0];
+            int indexMenor = 0;
+            int maior = idades[0];
+            int indexMaior = 0;
+            for (int i = 1; i < idades.length; i++) {
+                if (idades[i] > maior) {
+                    maior = idades[i];
+                    indexMaior = i;
+                } else if (idades[i] < menor) {
+                    menor = idades[i];
+                    indexMenor = i;
+                }
+            }
+
+            System.out.println("Vetor de idades = ");
+            for (int i = 0; i < idades.length; i++) {
+                System.out.println(idades[i] + " ");
+            }
+            System.out.println();
+
+            System.out.println("Menor idade: " + menor);
+            System.out.println("Indice menor idade: " + indexMenor);
+            System.out.println("Maior idade: " + maior);
+            System.out.println("Indice maior idade: " + indexMaior);
         
 
         //19  calcular a media e se for maior que 7 o aluno está aprovado
@@ -899,15 +938,11 @@ public class ExerciciosAula19 {
             System.out.println("1 = " + fatorital);
         }
             */
-            // exercicio 3 corrigido
+            // exercicio 18 corrigido
 
 
                 
-
-                
-    
-            
-        
+      
 
 
 
