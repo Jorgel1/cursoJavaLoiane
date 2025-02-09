c5bdbb7cd7e67a03647f07acba90036714293624
Jorgel1 <jorgegothicmetal@gamail.com>
2025-02-09 16:51:40 -0300

    finalizado o video 45/98 debug

--- src/com/loiane/cursojava/aula22/DebugIntelij.java
+++ src/com/loiane/cursojava/aula22/DebugIntelij.java
@@ -2,11 +2,11 @@ package com.loiane.cursojava.aula22;
 
 public class DebugIntelij {
     public static void main(String[] args) {
-        int [] notas = new int[3];
+        int[] notas = new int[3];
 
         for (int i = 0; i < 4; i++) {
             notas[i] = i;
         }
     }
 }
-// finalizado o video 44/98 debug
+// finalizado o video 45/98 debug
