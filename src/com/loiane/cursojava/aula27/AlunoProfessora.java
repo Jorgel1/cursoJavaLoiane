9cc5cbb61a9ee665db2b9e40c52a30db11226613
jorramos <95020694+Jorgel1@users.noreply.github.com>
2025-03-29 06:07:18 +0000

    exerc 03 corrigido  53/98

--- src/com/loiane/cursojava/aula27/AlunoProfessora.java
+++ src/com/loiane/cursojava/aula27/AlunoProfessora.java
@@ -16,23 +16,27 @@ public class AlunoProfessora {
         for (int i = 0; i < notasDisciplinas.length; i++) {
             System.out.println("Notas da disciplina " + nomeDisciplinas[i]);
             for (int j = 0; j < notasDisciplinas[i].length; j++) {
-                System.out.println(notasDisciplinas[i][j] + " ");
+                System.out.print(notasDisciplinas[i][j] + " ");
             }
             System.out.println();
         }
     }
 
     boolean verificarAprovado(int indice) {
-        double soma = 0;
-        for (int i = 0; i < notasDisciplinas[i].length; i++) {
-            soma += notasDisciplinas[indice][i];
-        }
-        double media = soma / 4;
 
-        if (media >= 7) {
+        if (obterMedia(indice) >= 7) {
             return true;
         }
         return false;
 
+    }
+
+    double obterMedia(int indice){
+        double soma = 0;
+        for (int i = 0; i < notasDisciplinas[indice].length; i++) {
+            soma += notasDisciplinas[indice][i];
+        }
+        double media = soma / 4;
+        return media;
     }
 }
