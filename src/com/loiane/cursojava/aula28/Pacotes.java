c5616308a09a8b08ced5603810e6053d7bf6114e
jorramos <95020694+Jorgel1@users.noreply.github.com>
2025-08-10 01:07:01 +0000

    Tutorial de como importar pacotes 55/98

---
+++ src/com/loiane/cursojava/aula28/Pacotes.java
@@ -0,0 +1,23 @@
+package com.loiane.cursojava.aula28;
+
+import com.loiane.cursojava.aula24.Lampada;
+import com.loiane.cursojava.aula27.Aluno;
+
+import com.loiane.cursojava.aula26.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        Carro carro;
+        Carro carro2;
+
+        Lampada lampada;
+
+        Aluno aluno;
+
+        /*
+         Essa aula explica a respeito de importar pacotes
+         Tutorial de como importar pacotes 55/98
+        */
+        
+    }
+}
