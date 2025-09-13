4779b9870ce58d871bb3363e60ae28a9851aa9bf
jorramos <95020694+Jorgel1@users.noreply.github.com>
2025-09-13 07:46:43 +0000

    Feito o exerc 1 aula 34 video 65/98

--- src/com/loiane/cursojava/aula33/ExercAula28a33.java
+++ src/com/loiane/cursojava/aula33/ExercAula28a33.java
@@ -103,43 +103,43 @@ public class ExercAula28a33 {
 
         // Versão da professora:
 
-            Scanner scan = new Scanner(System.in);
+        //     Scanner scan = new Scanner(System.in);
 
-            AlunoProfessora aluno = new AlunoProfessora();
+        //     AlunoProfessora aluno = new AlunoProfessora();
 
-            System.out.println("Entre com o nome do aluno");
-            aluno.setNome(scan.next());
+        //     System.out.println("Entre com o nome do aluno");
+        //     aluno.setNome(scan.next());
 
-            System.out.println("Entre com o nome do curso");
-            aluno.setNomeCurso(scan.next());
+        //     System.out.println("Entre com o nome do curso");
+        //     aluno.setNomeCurso(scan.next());
 
-            System.out.println("Entre com a matricula");
-            aluno.setMatricula(scan.next());
+        //     System.out.println("Entre com a matricula");
+        //     aluno.setMatricula(scan.next());
 
-            // aluno.nomeDisciplinas = new String[3];
-            for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
-                System.out.println("Entre com o nome da disciplina " + i);
-                aluno.setNomeDisciplinaPos(i, scan.next());
-            }
+        //     // aluno.nomeDisciplinas = new String[3];
+        //     for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
+        //         System.out.println("Entre com o nome da disciplina " + i);
+        //         aluno.setNomeDisciplinaPos(i, scan.next());
+        //     }
 
-            for (int i = 0; i < aluno.getNotasDisciplinas().length; i++) {
-                System.out.println("Obtendo notas da disciplina " + aluno.getNomeDisciplinas()[i]);
-                for (int j = 0; j < aluno.getNotasDisciplinas()[i].length; j++) {
-                    System.out.println("Entre com a nota " + (j + 1));
-                    aluno.setNomePosIJ(i, j, scan.nextDouble());
-                }
-            }
+        //     for (int i = 0; i < aluno.getNotasDisciplinas().length; i++) {
+        //         System.out.println("Obtendo notas da disciplina " + aluno.getNomeDisciplinas()[i]);
+        //         for (int j = 0; j < aluno.getNotasDisciplinas()[i].length; j++) {
+        //             System.out.println("Entre com a nota " + (j + 1));
+        //             aluno.setNomePosIJ(i, j, scan.nextDouble());
+        //         }
+        //     }
 
-            aluno.mostrarInfo();
+        //     aluno.mostrarInfo();
 
-            for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
-                if (aluno.verificarAprovado(i)) {
-                    System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - foi aprovado");
-                } else {
-                    System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - reprovado");
-                }
-            }
-        }
+        //     for (int i = 0; i < aluno.getNomeDisciplinas().length; i++) {
+        //         if (aluno.verificarAprovado(i)) {
+        //             System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - foi aprovado");
+        //         } else {
+        //             System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - reprovado");
+        //         }
+        //     }
+        // }
 
         // --------------------------------------------------------------------------
 
